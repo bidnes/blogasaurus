ce081a3712478c0c4ca6f3fb93d8889e341fa862
bidnes <dmlj74@gmail.com>
2024-03-01 16:31:23 -0500

    complete blog starting point

---
+++ side.py
@@ -0,0 +1,7 @@
+class User(db.Model, UserMixin):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    email: Mapped[str] = mapped_column(String(100), unique=True)
+    password: Mapped[str] = mapped_column(String(100))
+    name: Mapped[str] = mapped_column(String(100))
+    posts = relationship("BlogPost", back_populates="author")
